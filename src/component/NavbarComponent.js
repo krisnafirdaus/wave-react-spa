49da90c749945e062c9517d23ef1c5e981dd1e13
krisnafirdaus <krisnafirdaus@gmail.com>
2022-03-17 21:28:45 +0800

    add new feature

---
+++ src/component/NavbarComponent.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+
+class NavbarComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  render() {
+    return (
+      <Navbar color="dark" dark expand="lg">
+        <NavbarBrand href="/">Binar Academy</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem className="px-2">
+              <Link to="/">Home</Link>
+            </NavItem>
+            <NavItem className="px-2">
+              <Link to="/about">About</Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default NavbarComponent;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,34 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Container } from "reactstrap";
+import NavbarComponent from "../component/NavbarComponent";
+
+class Post extends Component {
+  render() {
+    const { title } = this.props;
+    return <h5>{title}</h5>;
+  }
+}
 
 class Home extends Component {
+  state = { posts: [] };
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((result) => {
+        this.setState({ posts: result });
+      });
+  }
   render() {
+    const { posts } = this.state;
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+      <Fragment>
+        <NavbarComponent />
+        <Container className="p-4">
+          {posts.map((i) => (
+            <Post title={i.title} />
+          ))}
+        </Container>
+      </Fragment>
     );
   }
 }
